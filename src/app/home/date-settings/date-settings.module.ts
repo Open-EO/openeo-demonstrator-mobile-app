a5a8db5861a2c8ead75e30e2cae65677f0b3c4d6
Joep Neijt <joep.neijt@solenix.ch>
2020-08-03 17:23:37 +0200

    Fix page navigation

--- src/app/home/date-settings/date-settings.module.ts
+++ src/app/home/date-settings/date-settings.module.ts
@@ -3,9 +3,24 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { DateSettingsPage } from './date-settings.page';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: DateSettingsPage
+    }
+];
 
 @NgModule({
-    imports: [CommonModule, FormsModule, IonicModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes),
+        SharedModule
+    ],
     declarations: [DateSettingsPage]
 })
 export class DateSettingsPageModule {}
